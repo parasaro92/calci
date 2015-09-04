eed4a680e727b35fdd8ad09b3ae45cec8d77b303
paras <rockypro92@gmail.com>
2015-09-04 18:49:55 +0530

    Creating the digit

---
+++ assets/scripts/calci.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#keys .digit').click(function() {
+    $('#preview').html($('#preview').html() + this.dataset.digit);
+  });
+$('#delete').click(function() {
+  $('#preview').html($('#preview').html().slice(0, -1));
+});
+});
