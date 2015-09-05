7c37e0439364440caea8c17a4b35b56485c9ff9d
paras <rockypro92@gmail.com>
2015-09-05 22:13:05 +0530

    handling input digits with keyboard and Corner cases

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,6 +1,8 @@
-var calculator {
+var calculator = {
+
   init: function() {
     $('#calculator .input').click(function() {
+
       if(this.dataset.keyType == "digit") {
         calculator.handleInput(this.dataset.digit);
       } else if (this.dataset.keyType == "operator") {
@@ -11,16 +13,20 @@ var calculator {
         calculator.evaluateResult();
       }
     });
-    ('#calculator #delete').dblclick(function() {
+
+    $('#calculator #delete').dblclick(function() {
         calculator.clearResult();
     });
+
     ['0','1','2','3','4','5','6','7','8','9','/','*','+','-'].forEach(function(digit) {
       $(document).bind('keyup', digit ,function() {
         calculator.handleInput(digit);
       });
     });
+    
     $(document).bind('keyup', 'backspace' ,function() {
         calculator.handleDelete();
+      });
     $(document).bind('keyup', 'shift+=' ,function() {
         calculator.handleInput('+');
       });
@@ -30,22 +36,30 @@ var calculator {
       });
     });
   },
+  /*
   handleInput: function(input) {
-      $('#preview').html($('#preview').html() + input);  
+    $('#preview').html($('#preview').html() + input);  
   },
+  */
+  handleInput: function(input) {
+    $('#preview').html($('#preview').html() + input);
+  },
+  
   handleDelete: function() {
       $('#preview').html($('#preview').html().slice(0, -1));
       if($('#preview').html().length == 0) {
         calculator.clearResult();
       }
   },
+  
   evaluateResult: function() {
       $('#result').html(eval($('#preview').html()));
-},
+  },
+  
   clearResult: function() {
       $('#result').html('');
   }
-};
+}
 
 $(document).ready(function() {
   calculator.init();
