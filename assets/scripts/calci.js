6b982c185b9ec1310c96d84a9b999bc188d24b0a
paras <rockypro92@gmail.com>
2015-09-05 15:57:29 +0530

    User can enter  digits using clicking

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -14,4 +14,15 @@ $(document).ready(function() {
     $('#preview').html($('#preview').html() + input);
   }
 });
+
+  ('#calculator #delete').dblclick(function() {
+    $('#preview').html('');
+    $('#result').html('');
+  });
+
+  ['0','1','2','3','4','5','6','7','8','9'].forEach(function() {
+    $(document).bind('keyup',digit,function() {
+    $('#preview').html($('#preview').html() + digit);
+    });
+  });
 });
