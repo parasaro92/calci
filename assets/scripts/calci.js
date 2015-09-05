e576d001542cedb0198a02f4019fd7b58fc39747
paras <rockypro92@gmail.com>
2015-09-05 19:08:29 +0530

    Modularisation

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,37 +1,52 @@
-function handleInput(input) {
-    $('#preview').html($('#preview').html() + input);  
-}
-
-function handleInput(input) {
-    $('#preview').html($('#preview').html().slice(0, -1));
-}
-
-function handleInput(input) {
-    $('#result').html(eval($('#preview').html()));
-}
+var calculator {
+  init: function() {
+    $('#calculator .input').click(function() {
+      if(this.dataset.keyType == "digit") {
+        calculator.handleInput(this.dataset.digit);
+      } else if (this.dataset.keyType == "operator") {
+        calculator.handleInput(this.dataset.operator);
+      } else if (this.dataset.keyType == "delete") {
+        calculator.handleDelete();
+      } else if (this.dataset.keyType == "equals") {
+        calculator.evaluateResult();
+      }
+    });
+    ('#calculator #delete').dblclick(function() {
+        calculator.clearResult();
+    });
+    ['0','1','2','3','4','5','6','7','8','9','/','*','+','-'].forEach(function(digit) {
+      $(document).bind('keyup', digit ,function() {
+        calculator.handleInput(digit);
+      });
+    });
+    $(document).bind('keyup', 'backspace' ,function() {
+        calculator.handleDelete();
+    $(document).bind('keyup', 'shift+=' ,function() {
+        calculator.handleInput('+');
+      });
+    ['=','return'].forEach(function(key) {
+    $(document).bind('keyup', key ,function() {
+        calculator.evaluateResult();
+      });
+    });
+  },
+  handleInput: function(input) {
+      $('#preview').html($('#preview').html() + input);  
+  },
+  handleDelete: function() {
+      $('#preview').html($('#preview').html().slice(0, -1));
+      if($('#preview').html().length == 0) {
+        calculator.clearResult();
+      }
+  },
+  evaluateResult: function() {
+      $('#result').html(eval($('#preview').html()));
+},
+  clearResult: function() {
+      $('#result').html('');
+  }
+};
 
 $(document).ready(function() {
-  $('#calculator .input').click(function() {
-    var input;
-    if(this.dataset.keyType == "digit") {
-      handleInput(this.dataset.digit);
-    } else if (this.dataset.keyType == "operator") {
-      handleInput(this.dataset.operator);
-    } else if (this.dataset.keyType == "delete") {
-      handleDelete();
-    } else if (this.dataset.keyType == "equals") {
-      evaluateResult();
-    }
-});
-
-  ('#calculator #delete').dblclick(function() {
-    $('#preview').html('');
-    $('#result').html('');
-  });
-
-  ['0','1','2','3','4','5','6','7','8','9'].forEach(function() {
-    $(document).bind('keyup', digit ,function() {
-    handleInput(digit);
-    });
-  });
+  calculator.init();
 });
