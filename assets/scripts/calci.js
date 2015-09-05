82d0112d541867004e1a972562b6f6a559a04179
paras <rockypro92@gmail.com>
2015-09-05 12:26:46 +0530

    Basic calculator without handling corner cases

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  $('#keys .digit').click(function() {
-    $('#preview').html($('#preview').html() + this.dataset.digit);
-  });
-$('#delete').click(function() {
-  $('#preview').html($('#preview').html().slice(0, -1));
+  $('#calculator .input').click(function() {
+    var input;
+    if(this.dataset.keyType == "digit") {
+      input = this.dataset.digit;
+    } else if (this.dataset.keyType == "operator") {
+      input = this.dataset.operator;
+    } else if (this.dataset.keyType == "delete") {
+      $('#preview').html($('#preview').html().slice(0, -1));
+    } else if (this.dataset.keyType == "equals") {
+      $('#result').html(eval($('#preview').html()));
+    }
+    if(typeof(input) != 'undefined') {
+    $('#preview').html($('#preview').html() + input);
+  }
 });
 });
